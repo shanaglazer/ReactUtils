2a0de04c0c375af7bc440b61d17babae055bb56a
shanaglazer <shanaglazer1@gmail.com>
2024-11-13 10:20:39 +0200

    end 28

--- src/DataUtil.ts
+++ src/DataUtil.ts
@@ -1,37 +1,48 @@
 import { FieldValues } from "react-hook-form";
 
+const hasErrorMessage = <T>(data: T): data is T & { errorMessage?: string } => {
+    return (data as { errorMessage?: string }).errorMessage !== undefined;
+};
+
 export function createAPI(baseurl:string, sessionkey:string){
 
+    async function request<T>(url:string, options:RequestInit):Promise<T> {
+        url = baseurl + url;
+        const r = await fetch(url, {
+            ...options,
+            headers:{
+                ...options.headers,
+                "Authorization":`Bearer ${sessionkey}`
+            }
+        });
+        const data = await r.json();
+        if(!r.ok){
+            if(r.status == 401){
+                throw new Error("unauthorized");
+            }
+            else if(!hasErrorMessage(data) || !data.errorMessage){
+                throw new Error('Error calling API');
+            }
+        }
+        return data;
+    };
+
 async function fetchData<T>(url:string):Promise<T> {
-    url = baseurl + url;
-    const r = await fetch(url, {
-        headers:{"Authorization":`Bearer ${sessionkey}`}
-    });
-    const data = await r.json();
-    return data;
+    return request(url, {});
 };
 
 async function postData<T>(url:string, form:FieldValues):Promise<T> {
-    url = baseurl + url;
-    const r = await fetch(url, {method:"POST",
+    return request(url, {
+        method:"POST",
         body: JSON.stringify(form),
         headers:{
             "Content-Type":"application/json",
-            "Authorization":`Bearer ${sessionkey}`
                 }
-            });
-    const data = await r.json();
-    return data;
+    });
 };
 
 async function deleteData<T>(url:string, ):Promise<T> {
-    url = baseurl + url;
-    const r = await fetch(url, {
-        method: "DELETE",
-        headers:{"Authorization":`Bearer ${sessionkey}`}
-    });
-    const data = await r.json();
-    return data;
+    return request(url, {method: "DELETE"});
 };
 
     return{fetchData, deleteData, postData,}
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,7 @@
 import { useForm } from "react-hook-form";
 import { getUserStore } from "./UserStore";
+import { useState } from "react";
+import { useStore } from "zustand";
 
 type forminput = { username: string; password: string };
 
@@ -15,12 +17,26 @@ export default function Login() {
   const logout = useUserStore((state) => state.logout);
   const username = useUserStore((state) => state.userName);
   const isLoggedIn = useUserStore((state) => state.isLoggedIn);
-  const onSubmit = async (data: forminput) =>
-    await login(data.username, data.password);
+  const [crashmsg, setCrashnsg] = useState("");
+  const errormsg = useUserStore((state) => state.errorMessage);
+
+  const onSubmit = async (data: forminput) => {
+    try {
+      setCrashnsg("");
+      await login(data.username, data.password);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setCrashnsg(error.message);
+      } else {
+        setCrashnsg("error...");
+      }
+    }
+  };
 
   return (
     <>
       <div>Logged In = {isLoggedIn.toString()}</div>
+      <h2 className="text-danger">{crashmsg || errormsg}</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Username</label>
         <input
